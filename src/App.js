387a4cd8dfcb6bc827b93e168ce195fa9bb55c6d
FranHercules <00079517@uca.edu.sv>
2022-10-27 14:58:20 -0600

    Agregando cambios de mi local

--- src/App.js
+++ src/App.js
@@ -26,18 +26,19 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar/>
       <Routes>
-        <Route index path="/" element={<HomePage/>}/>
-        <Route path="/reserva" element={<BuscarCancha/>}/>
-        <Route path="/reserva/:slug" element={<Reserva/>}/>
-        <Route path="/reserva/pagar" element={<Pagar/>}/>
-        <Route path="/reserva/pagar/exitoso" element={<ReservaResultado/>}/>
-        <Route path="/torneos/*" element={<Torneos/>}>
-          <Route path="todos" element={<TodosTorneos torneo={service_torneos}/>}/>
-          <Route path="mis-torneos" element={<TodosTorneos torneo={service_torneosm}/>}/>
+        <Route path="/" element={<NavBar/>}>
+          <Route index element={<HomePage/>}/>
+          <Route path="/reserva" element={<BuscarCancha/>}/>
+          <Route path="/reserva/:slug" element={<Reserva/>}/>
+          <Route path="/reserva/pagar" element={<Pagar/>}/>
+          <Route path="/reserva/pagar/exitoso" element={<ReservaResultado/>}/>
+          <Route path="/torneos/*" element={<Torneos/>}>
+            <Route path="todos" element={<TodosTorneos torneo={service_torneos}/>}/>
+            <Route path="mis-torneos" element={<TodosTorneos torneo={service_torneosm}/>}/>
+          </Route>
+          <Route path="*" element={<NotFoundPage/>} />
         </Route>
-        <Route path="*" element={<NotFoundPage/>} />
       </Routes>
     </BrowserRouter>
   );
