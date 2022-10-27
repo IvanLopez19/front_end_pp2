95f628daf7220e9c106b75fa682c4376078d909c
FranHercules <00079517@uca.edu.sv>
2022-10-27 08:22:25 -0600

    Solucion de bug con rutas en la seccion de torneos

--- src/pages/Torneos.js
+++ src/pages/Torneos.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import {Link, Outlet} from "react-router-dom"
+import React, {useEffect} from 'react';
+import {NavLink, Outlet, useNavigate} from "react-router-dom";
 
 export default function Torneos() {
+    const navigate = useNavigate();
+    useEffect(()=>{
+        navigate('/torneos/todos')
+    },[])
+
+    //navegar()
     return (
         <div className="container-fluid">
             Lista de partidos por torneos
             <div className="card text-center">
                 <div className="card-header">
-                    <ul className="nav nav-tabs card-header-tabs">
-                        <li className="nav-item">
-                            <Link className="nav-link" aria-current="true" to="todos">Todos los torneos</Link>
+                    <ul className="nav nav-tabs card-header-tabs" role="tablist">
+                        <li className="nav-item" role="presentation">
+                            <NavLink className="nav-link" aria-current="page" to="todos" aria-selected="false">Todos los torneos</NavLink>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="mis-torneos">Mis torneos</Link>
+                        <li className="nav-item" role="presentation">
+                            <NavLink className="nav-link" to="mis-torneos" aria-selected="false">Mis torneos</NavLink>
                         </li>
                     </ul>
                 </div>
