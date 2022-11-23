e1fe016a62ae6cc3eea128cc371a93ba1b4eae33
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2022-11-22 22:20:35 -0600

    Ajustes en componente "Perfil"

--- src/Components/Perfil/Perfil.js
+++ src/Components/Perfil/Perfil.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import './perfil.css';
-import '../../data/perfilData.json'
+import perfiles from "../../data/perfilData.json";
+import imagenUsuario from "../../img/usuario.png"
 
 
-export default function Perfiles ({Perf}) {
+export default function Perfiles () {
     return (
-        <div className='container'>
-            <div className="card text-center">
-                <div className="card-header">
-                    PERFIL
-                </div>
-                <div className="card-body">
-                    <p>informacion usuario</p>
-                </div>
-            </div>
+        <div>
+            {
+                perfiles.map (infoPerfil => {
+                    return(
+                        <div className='container'>
+                            <div className="card text-center">
+                                <div className="card-header">
+                                    PERFIL
+                                </div>
+                                <div className="card-body">
+                                <img src={imagenUsuario} style={{width:100, height:100}}/>
+                                <p></p>
+                                    <div>
+                                        NOMBRE :
+                                        { infoPerfil.nombre}
+                                    </div>
+                                    <div>
+                                        EDAD :
+                                        { infoPerfil.edad}
+                                    </div>
+                                    <div>
+                                        CORREO :
+                                        { infoPerfil.correo}
+                                    </div>
+                                    <div>
+                                        TELEFONO :
+                                        { infoPerfil.telefono}
+                                    </div>
+                                    <div>
+                                        DIRECCION :
+                                        { infoPerfil.direccion}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
-
-export {Perfiles};
